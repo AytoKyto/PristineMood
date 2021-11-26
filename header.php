8827b12605ecd3096cfa0a6e847f6af81ac31e5d
julien_langeuse <julien@lanageuse.com>
2021-11-26 19:01:16 +0100

    ùmljkdfsmljds

--- header.php
+++ header.php
@@ -15,13 +15,13 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="https://use.typekit.net/eov5ijf.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 	<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
 	<?php wp_head(); ?>
@@ -30,7 +30,7 @@
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
 	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', '_s'); ?></a>
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 		<header id="masthead" class="siteHeader">
 			<nav id="site-navigation" class="nav">
 				<div class="wrapperNav">
--- home.php
+++ home.php
@@ -17,22 +17,34 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-    <div class="wrapperContact">
-        <div class="contact">
-            <div>
-                <p class="contactTitle">Contact us</p>
-                <p class="desContact">If you are a Serandipian member or partner check your email.
-                    Formstack link
-                    Are you interested to learn more about Pristine Mood ?
-                    Link to public form
-                    This is an exclusive launch for Serandipian Members and Partners.
-                    Click here to find out more about Serandipians.</p>
-            </div>
-            <div class="contact__form">
-                <?php echo do_shortcode('[contact-form-7 id="5" title="Contact form 1"]'); ?>
-            </div>
-        </div>
-    </div>
+	<!-- hero -->
+	<div class="heroWrapper">
+		<div class="heroContent">
+			<div class="heroContent-logo"><img class="svgLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/logohero.svg"/></div>
+			<h1>Seeding, sporting, blooming !</h1>
+			<h2>Turn our luxury travel industry <br/>
+				into a leading responsibility and <br/>
+				sustainability </h2>
+		</div>
+	</div>
+
+	<!-- Contact -->
+	<div class="wrapperContact">
+		<div class="contact">
+			<div>
+				<p class="contactTitle">Contact us</p>
+				<p class="desContact">If you are a Serandipian member or partner check your email.
+					Formstack link
+					Are you interested to learn more about Pristine Mood ?
+					Link to public form
+					This is an exclusive launch for Serandipian Members and Partners.
+					Click here to find out more about Serandipians.</p>
+			</div>
+			<div class="contact__form">
+				<?php echo do_shortcode( '[contact-form-7 id="5" title="Contact form 1"]' ); ?>
+			</div>
+		</div>
+	</div>
 
 </main><!-- #main -->
 
