def22f0df6a3dbe44f59f68d84bcc9722e591007
julien_langeuse <julien@lanageuse.com>
2021-12-02 08:48:15 +0100

    font

--- header.php
+++ header.php
@@ -21,9 +21,7 @@
 	<link rel="stylesheet" href="https://use.typekit.net/eov5ijf.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400,500,600&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-	<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
